8692e064c442b1b2adcd49dfa3a0c07ccf3bc1fc
Elena <elli_shii@mail.ru>
2022-06-24 15:05:05 +0300

    keyboard

--- src/keyboard.js
+++ src/keyboard.js
@@ -7,34 +7,72 @@ const keypress = require('keypress');
 // Управление.
 // Настроим соответствия нажатий на клавиши и действий в игре.
 
-const keyboard = {
-  q: () => console.log('q'),
-  w: () => console.log('w'),
-  e: () => console.log('e'),
-  r: () => console.log('r'),
-  t: () => console.log('t'),
-  y: () => console.log('y'),
-};
+class Keyboard {
+  constructor({ hero, boomerang }) {
+    this.hero = hero;
+    this.boomerang = boomerang;
+    this.keyboard = {
+      d: () => this.hero.moveRight(),
+      a: () => {
+        if (this.hero.position > 0) {
+          this.hero.moveLeft();
+        }
+      },
+      space: () => {
+        if (this.hero.position === this.boomerang.position) {
+          this.boomerang.moveRight();
+        }
+      },
+    };
+  }
+
+  runInteractiveConsole() {
+    keypress(process.stdin);
+    process.stdin.on('keypress', (ch, key) => {
+      if (key) {
+        // Вызывает команду, соответствующую нажатой кнопке.
+        if (key.name in this.keyboard) {
+          this.keyboard[key.name]();
+        }
+        // Прерывание программы.
+        if (key.ctrl && key.name === 'c') {
+          process.exit();
+        }
+      }
+    });
+    process.stdin.setRawMode(true);
+  }
+}
+// const keyboard = {
+//   q: () => console.log('q'),
+//   w: () => console.log('w'),
+//   e: () => console.log('e'),
+//   r: () => console.log('r'),
+//   t: () => console.log('t'),
+//   y: () => console.log('y'),
+// };
 
 // Какая-то функция.
 
-function runInteractiveConsole() {
-  keypress(process.stdin);
-  process.stdin.on('keypress', (ch, key) => {
-    if (key) {
-      // Вызывает команду, соответствующую нажатой кнопке.
-      if (key.name in keyboard) {
-        keyboard[key.name]();
-      }
-      // Прерывание программы.
-      if (key.ctrl && key.name === 'c') {
-        process.exit();
-      }
-    }
-  });
-  process.stdin.setRawMode(true);
-}
+// function runInteractiveConsole() {
+//   keypress(process.stdin);
+//   process.stdin.on('keypress', (ch, key) => {
+//     if (key) {
+//       // Вызывает команду, соответствующую нажатой кнопке.
+//       if (key.name in keyboard) {
+//         keyboard[key.name]();
+//       }
+//       // Прерывание программы.
+//       if (key.ctrl && key.name === 'c') {
+//         process.exit();
+//       }
+//     }
+//   });
+//   process.stdin.setRawMode(true);
+// }
 
 // Давай попробуем запустить этот скрипт!
 
-runInteractiveConsole();
+// runInteractiveConsole();
+
+module.exports = Keyboard;
