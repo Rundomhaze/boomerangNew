493a5855a5c25c2eb5e4d64cd62c6973620f5e6d
Alex <petenkov2822@gmail.com>
2022-06-25 14:57:22 +0300

    View fix

--- src/View.js
+++ src/View.js
@@ -1,7 +1,24 @@
 // Сделаем отдельный класс для отображения игры в консоли.
+const cfonts = require('cfonts');
+// --font [ console, block, simpleBlock, simple, 3d, simple3d, chrome, huge, shade, slick, grid, pallet, tiny ] 
+// --align [ left, center, right, top, bottom ]
+// --background blue [ transparent, black, red, green, yellow, blue, magenta, cyan, white, blackBright, redBright, greenBright, yellowBright, blueBright, magentaBright, cyanBright, whiteBright ]
+//  --gradient red,blue,green
+const style = {
+  font: 'slick',
+  align: 'center',
+  backgroundColor: 'transparent',
+  letterSpacing: 0,
+  gradient: 'blue,green',
+  lineHeight: 0,
+};
+
 
 class View {
-  constructor({ track, hero }) {
+  constructor({
+    track,
+    hero
+  }) {
     this.track = track;
     this.hero = hero;
   }
@@ -10,12 +27,22 @@ class View {
 
     // Тут всё рисуем.
     console.clear();
-    // console.log(this.game.track.join(''));
-    console.log(`${track.join('')}`);    
+    cfonts.say('B O O M E R A N G', style);
     console.log('\n\n');
-    console.log(`Created by "${yourTeamName}" with love`);
-    console.log(`YOUR SCORE: ${this.hero.countDie}`);
-
+    console.log('🪧             🚫               🪧                🚸              🪧                 ⛔                 🪧              🚫            🪧        ⛔             🚸                ⛔               🪧       🚫');
+    console.log('        🪧             🚸                ⛔               🪧               🚫                  🚸               ⛔              🪧                    🚫               🪧                🚸              🪧  ');
+    console.log('______________________________________________________________________________________________________________________________________________________________________________________________________________');
+    console.log('\n\n\n');
+    console.log(`${track.join('')}`);
+    console.log('\n\n\n');
+    console.log('_______________________________________________________________________________________________________________________________________________________________________________________________________________');
+    console.log('\n');
+    console.log('🪧             🚫               🪧                🚸              🪧                 ⛔                 🪧              🚫            🪧        ⛔             🚸                ⛔               🪧        🚫');
+    console.log('        🪧             🚸                ⛔               🪧               🚫                  🚸               ⛔              🪧                    🚫               🪧                🚸              🪧   ');
+    console.log('\n\n\n');
+    console.log(`                                                                               Created by ${yourTeamName} with love`);
+    console.log(`                                                                               YOUR SCORE: ${this.hero.countDie}`);
+    console.log('\n\n\n');
   }
 }
 
